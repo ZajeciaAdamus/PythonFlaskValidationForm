a8bbb49a96e16f249ef1143802ed69d457813fa4
Paweł Adamus <zajecia.adamus@gmail.com>
2023-03-05 19:20:58 +0100

    code readability fix, require password to be AnyOf() specific words

--- PythonFlaskValidationForm/app.py
+++ PythonFlaskValidationForm/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 # potrzebujemy do wykonania formularza pole tekstowe i pole dla hasla
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, AnyOf
 
 # podanie sciezki do template'ow HTML
 app = Flask(__name__, template_folder='templates')
@@ -11,8 +11,13 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired('Username is required'), Length(min=5,max=10, message='Type 5 to 10 characters.')])
-    password = PasswordField('password', validators=[InputRequired('Password is required'), Length(min=5,max=10, message='Type 5 to 10 characters.')])
+    username = StringField('username',
+                             validators=[InputRequired('Username is required'),
+                             Length(min=5,max=10, message='Type 5 to 10 characters.')])
+    password = PasswordField('password',
+                             validators=[InputRequired('Password is required'),
+                             Length(min=5,max=10, message='Type 5 to 10 characters.'),
+                             AnyOf(values=['password','secret'])])
 
 # Route do strony glownej
 @app.route('/')

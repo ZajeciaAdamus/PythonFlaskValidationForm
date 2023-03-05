20c65b38220742d25c7f35730a0b2f8db0ad8245
Paweł Adamus <zajecia.adamus@gmail.com>
2023-03-05 19:05:28 +0100

    custom error handling

--- PythonFlaskValidationForm/app.py
+++ PythonFlaskValidationForm/app.py
@@ -1,10 +1,8 @@
-from tokenize import String
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms.validators import InputRequired
-
 # potrzebujemy do wykonania formularza pole tekstowe i pole dla hasla
 from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
 
 # podanie sciezki do template'ow HTML
 app = Flask(__name__, template_folder='templates')
@@ -13,8 +11,8 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired()])
-    password = PasswordField('password', validators=[InputRequired()])
+    username = StringField('username', validators=[InputRequired(message='Username is required.')])
+    password = PasswordField('password', validators=[InputRequired(message='Password is required.')])
 
 # Route do strony glownej
 @app.route('/')

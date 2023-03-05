59a728e4588357b438ac87d8d7dbb47257051008
Paweł Adamus <zajecia.adamus@gmail.com>
2023-03-05 19:12:05 +0100

    require 5 to 10 characters in username  and password

--- PythonFlaskValidationForm/app.py
+++ PythonFlaskValidationForm/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 # potrzebujemy do wykonania formularza pole tekstowe i pole dla hasla
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 
 # podanie sciezki do template'ow HTML
 app = Flask(__name__, template_folder='templates')
@@ -11,8 +11,8 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(message='Username is required.')])
-    password = PasswordField('password', validators=[InputRequired(message='Password is required.')])
+    username = StringField('username', validators=[InputRequired('Username is required'), Length(min=5,max=10, message='Type 5 to 10 characters.')])
+    password = PasswordField('password', validators=[InputRequired('Password is required'), Length(min=5,max=10, message='Type 5 to 10 characters.')])
 
 # Route do strony glownej
 @app.route('/')
